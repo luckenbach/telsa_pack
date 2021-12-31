690a05ab0080083f35f7bfd4a172a1663e741f53
Zach Hilliard <zach.hilliard@cyxtera.com>
2021-12-31 16:15:31 -0600

    moving vent over to _run()

--- actions/vent_windows.py
+++ actions/vent_windows.py
@@ -1,7 +1,7 @@
 from lib.action import BaseAction
 
 class ventWindows(BaseAction):
-    def run(self):
+    def _run(self, *args, **kwargs):
         vent = self.car.command('WINDOW_CONTROL', command='vent', lat=0, lon=0)
         if vent:
             return True, 'Vented'
