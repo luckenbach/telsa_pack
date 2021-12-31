16b939145ea592a72a87e0c04145dbfd4fd51c4a
Zach Hilliard <zach.hilliard@cyxtera.com>
2021-12-31 02:48:26 -0600

    Adding close action

---
+++ actions/close_window.py
@@ -0,0 +1,21 @@
+from st2common.runners.base_action import Action
+from util import get_context
+
+
+class closeWindows(Action):
+    def run(self, car_name):
+        user = self.config.get('tesla_pack_user', None)
+        if user:
+            t = get_context(user=user)
+        else:
+            return False, 'User not defined'
+        if car_name:
+            c = next(item for item in t.vehicle_list() if item["display_name"].lower() == car_name.lower())
+        else:
+            c = t.vehicle_list().pop
+        if c:
+            vent = c.command('WINDOW_CONTROL', command='close', lat=0, lon=0)
+            if vent:
+                return True, 'Closed'
+            else:
+                return False, 'Failed'
