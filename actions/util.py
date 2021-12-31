366d6667df804ca31e49f52a38dd27ffb155666f
Zach Hilliard <zach.hilliard@cyxtera.com>
2021-12-31 02:46:01 -0600

    utils fixes
    * corrected Client import
    * ensured the get_cache returns a dict
    * fixed () off cache_loader call

--- actions/util.py
+++ actions/util.py
@@ -1,15 +1,15 @@
 from teslapy import Tesla
-from st2 import Client
-
+from st2client.client import Client
+import json
 
 def get_cache():
     client = Client()
     secret = client.keys.get_by_name(name='tesla_pack_token', decrypt=True).value
     if secret:
-        return secret
+        return json.loads(secret)
     else:
-        return None
+        return {}
 
 def get_context(user):
-    with Tesla(user, cache_loader=get_cache()) as tesla:
+    with Tesla(user, cache_loader=get_cache) as tesla:
         return tesla
